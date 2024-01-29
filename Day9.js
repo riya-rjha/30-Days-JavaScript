7098e9067e9c214daa9f00611928d994fae1258f
riya-rjha <riyaranjanjha2006@gmail.com>
2024-01-30 02:47:54 +0530

    Incurring Changes

---
+++ Day9.js
@@ -0,0 +1,5 @@
+let products = [{ "x": 1 }, { "x": 0 }, { "x": -1 }]
+//when key is same, no sorting takes place
+console.log(products.sort());
+
+//Use sort function by taking two variables at a time
