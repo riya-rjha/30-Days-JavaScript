271429bddd61964c9d65fe2cefa071897132d4c8
riya-rjha <riyaranjanjha2006@gmail.com>
2024-01-31 02:06:22 +0530

    Promise Time Limit

--- Day9.js
+++ Day9.js
@@ -13,9 +13,38 @@ var fn = (key) => {
     return key.x; //value of 'x'
 }
 var sortBy = function (arr, fn) {
-    return arr.sort((a,b)=>fn(a)-fn(b));
+    return arr.sort((a, b) => fn(a) - fn(b));
 };
 console.log(sortBy(products, fn));
 
 //Alternative Method
-return arr.slice().sort((a, b) => fn(a) - fn(b)); //creates a new array
+// return arr.slice().sort((a, b) => fn(a) - fn(b)); //creates a new array
+
+
+//Promise Time Limit
+
+//If function completes within given time limit, resolve the promise 
+//If function exceeds given time limit, reject the promise
+
+fn = async (n) => {
+    await new Promise(res => setTimeout(res, 100));
+    return n * n;
+}
+
+var timeLimit = function (fn, t) {
+
+    return async function (...args) {
+        const originalFn = fn(...args);
+        const timeLimited = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                reject('Time Limit Exceeded!');
+            }, t);
+        });
+        return Promise.race([originalFn, timeLimited]);
+    }
+};
+
+inputs = [5]
+t = 50
+const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
